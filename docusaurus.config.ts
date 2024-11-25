22d123e6e18caf9d51253d1db9978c72147b8f7f
RyRy79261 <ryanjnoble@gmail.com>
2024-11-25 22:31:59 +0100

    fix: base url typo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://ryry79261.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/noble-notation/',
+  baseUrl: '/noble-notations/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
