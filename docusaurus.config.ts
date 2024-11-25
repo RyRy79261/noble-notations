f07702ed0d665a4d2b829bfa5ed188ce78a55eb5
RyRy79261 <ryanjnoble@gmail.com>
2024-11-25 22:26:57 +0100

    fix: base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://ryry79261.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/noble-notation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
